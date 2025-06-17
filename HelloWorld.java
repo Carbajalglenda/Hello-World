cc7ac7fb43512ed7133d03e065d9faffff5a060d
Carbajalglenda <carbajal.g833167@stu.sanjac.edu>
2025-06-17 03:51:23 +0000

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        // Stating my name 
+        System.out.println("Glenda Carbajal");
+        // My favorite food 
+        System.out.println("Pasta");
+        // Person i'd like to meet
+        System.out.println("Spiderman");
+        // Hated chore
+        System.out.println("Stove Cleaning");
+        // Rather be doing than this assingment
+        System.out.println("Swimming in the pool");
+        // The main() method in java means the entry point of execution for a program
+        // System.out.print is used to display output as it is a way to print text or objects to a terminal window
+        // Purpose of comments program is to let the other person know what the purpose of the code is without full digestion
+        
+    }
+}
